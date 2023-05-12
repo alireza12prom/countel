33db1ff497bb7d8e0c3b1065aeeeb964c8626deb
Alireza <alirezax69prom@gmail.com>
2023-05-12 03:36:32 +0330

    start point

---
+++ index.ts
@@ -0,0 +1,26 @@
+import { search_files, count_lines, path_validator, logger } from './lib';
+import { program } from 'commander';
+
+
+program
+  .version('0.0.1', '-v, --version')
+  .usage('dir [test]')
+  .parse(process.argv);
+
+main(program.args);
+
+function main(args: string[]) {
+  let path = args.at(0);
+  if (!path) {
+    logger.error('Error: please specify a path.');
+    return;
+  }
+  if (!path_validator(path)) {
+    logger.error(`Error: no such file or directory '${path}'.`);
+    return;
+  }
+
+  logger.message('Start counting, please wait... 🚀');
+  let total = (search_files(path).map(count_lines)).reduce((prev, curr) => prev + curr);
+  logger.result(`> Total lines << ${total.toLocaleString('en-US')} >>`);
+}
